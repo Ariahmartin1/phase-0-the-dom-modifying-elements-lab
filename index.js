bf2b8cbb78eeebcb08f5fcdc0700b9f6db421db1
zachariah martin <zachariahsmartin@gmail.com>
2022-09-17 14:46:19 -0400

    commit tree 27d6eb74a973c9547cec5216325776bba2cc9e4f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1")  
+newHeader.id = "victory"
+newHeader.textContent = "Zachariah is the champion"
+const element = document.getElementById("main");
+element.remove();
